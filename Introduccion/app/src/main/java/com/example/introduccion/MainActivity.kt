58937896107191c0734cb77858e1a2515dbd374d
Jokin <jokin.urteaga@ikasle.egibide.org>
2025-02-19 21:56:24 +0100

    Cambios Ej01

--- Introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
+++ Introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.introduccion
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,20 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        findViewById<Button>(R.id.bCalcular).setOnClickListener{
+            if (validar()){
+                val cocaCola = findViewById<EditText>(R.id.edCocacola).text
+                findViewById<TextView>(R.id.tvResultado).setText(cocaCola)
+            }
+        }
     }
+    fun validar(): Boolean{
+        if(findViewById<EditText>(R.id.etNombre).text.isEmpty()){
+            findViewById<TextView>(R.id.tvResultado).setText("Tienes que insertar tu nombre y apellido.")
+            return false
+        }
+        return true
+    }
+
 }
