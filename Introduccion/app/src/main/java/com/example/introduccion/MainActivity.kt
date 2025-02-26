412ab804ca83bfce7a7d246eaa6648453e29c269
Jokin <jokin.urteaga@ikasle.egibide.org>
2025-02-26 19:52:28 +0100

    Progreso

--- Introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
+++ Introduccion/app/src/main/java/com/example/introduccion/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.introduccion
 
 import android.os.Bundle
 import android.widget.Button
+import android.widget.CheckBox
+import android.widget.Checkable
 import android.widget.EditText
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -10,6 +12,7 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -21,15 +24,62 @@ class MainActivity : AppCompatActivity() {
         }
 
         findViewById<Button>(R.id.bCalcular).setOnClickListener{
-            if (validar()){
-                val cocaCola = findViewById<EditText>(R.id.edCocacola).text
-                findViewById<TextView>(R.id.tvResultado).setText(cocaCola)
+            var mayorDeEdad = false
+
+            val cocacola = findViewById<EditText>(R.id.edCocacola).text;
+            val kaslimon = findViewById<EditText>(R.id.edKasLimon).text;
+            val kasnaranja = findViewById<EditText>(R.id.edKasNaranja).text;
+            val redbull = findViewById<EditText>(R.id.edRedBull).text;
+            val cerveza = findViewById<EditText>(R.id.edCerveza).text;
+            val vino = findViewById<EditText>(R.id.edVino).text;
+
+            val cocacolaN: Int? = cocacola.toString().toIntOrNull()
+            val kaslimonN: Int? = kaslimon.toString().toIntOrNull()
+            val kasnaranjaN: Int? = kasnaranja.toString().toIntOrNull()
+            val redbullN: Int? = redbull.toString().toIntOrNull()
+            val cervezaN: Int? = cerveza.toString().toIntOrNull()
+            val vinoN: Int? = vino.toString().toIntOrNull()
+
+            val cantidades = listOf(cocacolaN, kaslimonN, kasnaranjaN, redbullN, cervezaN, vinoN)
+
+            if (vinoN != null){
+                if (vinoN > 0){
+                    mayorDeEdad = true
+                }
+            }
+            if (cervezaN != null){3
+                if (cervezaN > 0){
+                    mayorDeEdad = true
+                }
+            }
+
+            if (validar(mayorDeEdad)){
+                var resultado = 0;
+
+                for (cantidad in cantidades){
+                    if (cantidad != null){
+                        resultado += cantidad
+                    }
+                }
+
+                findViewById<TextView>(R.id.tvResultado).text = resultado.toString();
+
+                /*if (cocacolaN != null && kaslimonN != null && kasnaranjaN != null && redbullN != null && cervezaN != null && vinoN != null){
+                    resultado = cocacolaN + kaslimonN + kasnaranjaN + redbullN + cervezaN + vinoN;
+                    findViewById<TextView>(R.id.tvResultado).setText(resultado);
+                }else{
+                    findViewById<TextView>(R.id.tvResultado).setText("No se pudo calcular el resultado por insertar valores nulos o erroneos.")
+                }*/
             }
         }
     }
-    fun validar(): Boolean{
+    fun validar(esMayorDeEdad: Boolean): Boolean{
         if(findViewById<EditText>(R.id.etNombre).text.isEmpty()){
             findViewById<TextView>(R.id.tvResultado).setText("Tienes que insertar tu nombre y apellido.")
+            return false
+        }
+        if(!findViewById<CheckBox>(R.id.sEdad).isChecked && esMayorDeEdad){
+            findViewById<TextView>(R.id.tvResultado).setText("Tienes que ser mayor de edad para comprar alcohol.")
             return false
         }
         return true
