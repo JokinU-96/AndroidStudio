bb02ed9f8eb166f3035a1a962145d901fbcdaedb
Jokin <jokin.urteaga@ikasle.egibide.org>
2025-03-26 21:55:55 +0100

    Poco a poco

--- Ej4/app/src/main/java/com/example/ej4/ThirdFragment.kt
+++ Ej4/app/src/main/java/com/example/ej4/ThirdFragment.kt
@@ -9,8 +9,10 @@ import android.widget.CheckBox
 import android.widget.Toast
 import androidx.core.view.forEach
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.ej4.databinding.FragmentThirdBinding
 import com.example.ej4.modelo.Vehiculo
+import com.example.ej4.recyclerView.Adaptador
 
 
 class ThirdFragment : Fragment() {
@@ -20,6 +22,7 @@ class ThirdFragment : Fragment() {
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
+
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -34,29 +37,19 @@ class ThirdFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //Recorre la lista de vehiculos que hay en la actividad y agrega un checkbox programáticamente.
-        for ((indice, elemento) in (activity as MainActivity).vehiculos.withIndex()){
-            //Crea un nuevo CheckBox. Necesita como parametro un contexto. Para ello usamos context
-            val checkBox = CheckBox(context)
-            checkBox.text = elemento.marcaVehiculo
-            //Establece el id del CheckBox, que va a coincidir con la posición del vehiculo en la lista.
-            checkBox.id = indice
-            //Agrega el CheckBox al LinearLayout
-            binding.llVehiculos.addView(checkBox)
-        }
+        binding.rvVehiculos.layoutManager= LinearLayoutManager(context)
+        binding.rvVehiculos.adapter= Adaptador((activity as MainActivity).vehiculos)
 
-        var vehiculosComprados = ""
-
-        binding.btnComprar.setOnClickListener{
-            binding.llVehiculos.forEach { control ->
-                if (control is CheckBox){
-                    vehiculosComprados += "${control.text}\n"
-                    (activity as MainActivity).usuario?.vehiculos?.add((activity as MainActivity).vehiculos.get(control.id))
-                }
-            }
-
-            Toast.makeText(context, vehiculosComprados, Toast.LENGTH_LONG).show()
-        }
+//        binding.btnComprar.setOnClickListener{
+//            binding.llVehiculos.forEach { control ->
+//                if (control is CheckBox){
+//                    vehiculosComprados += "${control.text}\n"
+//                    (activity as MainActivity).usuario?.vehiculos?.add((activity as MainActivity).vehiculos.get(control.id))
+//                }
+//            }
+//
+//            Toast.makeText(context, vehiculosComprados, Toast.LENGTH_LONG).show()
+//        }
 
 
     }
--- Ej4/app/src/main/java/com/example/ej4/recyclerView/Adaptador.kt
+++ Ej4/app/src/main/java/com/example/ej4/recyclerView/Adaptador.kt
@@ -1,24 +1,39 @@
 package com.example.ej4.recyclerView
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.core.os.bundleOf
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ej4.R
+import com.example.ej4.ThirdFragment
 import com.example.ej4.modelo.Vehiculo
 import com.example.ej4.databinding.RecyclerviewVehiculoBinding
 
 class Adaptador(var vehiculos: MutableList<Vehiculo>):RecyclerView.Adapter<Adaptador.VehiculoVH>(){
     inner class VehiculoVH(val binding: RecyclerviewVehiculoBinding): RecyclerView.ViewHolder(binding.root){
         //Aquí dentro pongo un listener para configurar la acción de los botones.
+        var posicion: Int = 0
+        init {
+            binding.rviVerVehiculo.setOnClickListener{
+                val miBundle = bundleOf("posicion" to posicion)
+                //Llevo al usuario a la ventana donde se muestr más información sobre el vehículo.
+                binding.rviVerVehiculo.findNavController().navigate(R.id.action_fourthFragment_to_ThirdFragment /*De donde a donde va*/, miBundle/*miBundle*/)
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VehiculoVH {
-        TODO("Not yet implemented")
+        val binding = RecyclerviewVehiculoBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return VehiculoVH(binding)
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return vehiculos.count()
     }
 
     override fun onBindViewHolder(holder: VehiculoVH, position: Int) {
-        TODO("Not yet implemented")
+        holder.binding.rviMarca.text = "${vehiculos[position].marcaVehiculo}"
+        holder.posicion = position
     }
 }
